09ee927bcd6bfb6e6907b995e3df668fb42f1bd0
mewsickx <tshedupghissing805@gmail.com>
2026-08-09 20:08:09 +0530

    added pseudocode for postfix evaluation

---
+++ postfix_evaluation.cpp
@@ -0,0 +1,4 @@
+//If input is operand, put it in the stack
+//If input is operator, then x1=pop(), x2=pop(), result=x2 operator x1, push(result)
+//By the end, the stack should only contain one value that being the result.    
+//then result=pop() and print the result
